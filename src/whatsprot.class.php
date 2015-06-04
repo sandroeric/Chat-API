524ad95bee5e151c375eb78ed072c4b018c12b99
Sandro Santos <sandro.s@hostdime.com.br>
2015-06-04 11:43:11 -0300

    get the participants of some group function

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -1320,6 +1320,42 @@ class WhatsProt
 
         return $groupId;
     }
+    
+        /**
+     * Send a request to return a list of people participating in a specific
+     * group.
+     *
+     * @param  string $gjid The specific group id
+     */
+    public function sendGetGroupsParticipants($gjid)
+    {
+        $msgId = $this->createMsgId("getgroupparticipants");
+
+        $child = new ProtocolNode("list", null, null, null);
+        $node = new ProtocolNode("iq", array(
+            "id" => $msgId,
+            "type" => "get",
+            "xmlns" => "w:g",
+            "to" => $this->getJID($gjid)
+                ), array($child), null);
+        $this->sendNode($node);
+
+        $this->waitForServer($msgId);
+    }
+    
+    /**
+     * Pull from the socket, and place incoming messages in the message queue.
+     */
+    public function pollMessages($autoReceipt = true)
+    {
+        $stanza = $this->readStanza();
+        while($stanza)
+        {
+            $this->processInboundData($stanza, $autoReceipt);
+            $stanza = $this->readStanza();
+        }
+    }
+
 
     /**
      * Change group's subject.
